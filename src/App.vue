a1de26af86eb17fdfcf47d09b7a6050e6a668481
Pshtiwan Mahmood <kardoey@gmail.com>
2024-02-02 19:55:15 +0300

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup>
 import ScreeningForm from  './components/ScreeningForm.vue';
+const year = new Date();
+
 </script>
 
 <template>
@@ -24,8 +26,9 @@ import ScreeningForm from  './components/ScreeningForm.vue';
 
 
 
-    <hr class="bg-secondary">
-    <div class="text-center px-3 ">
+    <hr class="bg-secondary mt-5  " />
+    <div class="d-flex text-center text-lg-start flex-column flex-lg-row justify-content-between px-3 mt-2 mb-4">
+      <p>©{{year.getFullYear()}} وەزارەتی تەندروستی حوکمەتی هەرێمی کوردستان, گشت مافەکانی پارێزراوە. </p>
       <p>Developed by <a href="https://github.com/drpshtiwan" target="_blank">Dr. Pshtiwan</a></p>
     </div>
 
--- src/components/ScreeningForm.vue
+++ src/components/ScreeningForm.vue
@@ -4,73 +4,74 @@
     <p class="text-secondary ">ئەم فۆڕمەی خوارەوە پڕبکەوە بۆ زانینی ئەو پشکنینە پێشوەختانەی پێویستە ئەنجامی بدەیت بۆ
       شێرپەنجە باوەکان.</p>
     <hr class="my-4">
-    <div class="row">
-      <div class="col-12 col-md-6 col-lg-4 col-xl-3 mb-3">
-        <label for="cityInput" class="form-label text-black h6">شوێنی نیشتەجێبوون</label>
-        <select v-model="city" id="cityInput" class="form-select">
-          <option v-for="city in cities" :value="city.value">{{ city.name }}</option>
-        </select>
-      </div>
-
-      <div class="col-12 col-md-6 col-lg-4 col-xl-2 mb-3">
-        <label for="ageInput" class="form-label text-black h6">تەمەن</label>
-        <input v-model="age" required type="number" class="form-control " id="ageInput" placeholder="تەمەن بە ژمارە">
-      </div>
-      <div class="col-12 col-md-6 col-lg-4 col-xl-2 mb-3">
-        <label for="genderInput" class="form-label text-black h6">ڕەگەز</label>
-        <select v-model="gender" id="genderInput" class="form-select">
-          <option value="male">نێر</option>
-          <option value="female">مێ</option>
-        </select>
-      </div>
-
-      <div class="col-12 col-md-6 col-lg-4 col-xl-3 mb-3">
-        <label for="maritalStatusInput" class="form-label text-black h6">دۆخی خێزانی</label>
-        <select v-model="maritalStatus" id="maritalStatusInput" class="form-select">
-          <option value="single">سەڵت</option>
-          <option value="married">خێزاندار</option>
-        </select>
-      </div>
-
-      <div class="col-12 col-md-6 col-lg-4 col-xl-2 mb-3">
-        <label for="smokingInput" class="form-label text-black h6">جگەرەت کێشاوە؟</label>
-        <select v-model="smoking" id="smokingInput" class="form-select">
-          <option value="true">بەڵی</option>
-          <option value="false">نەخێر</option>
-        </select>
-      </div>
-    </div>
-    <hr class="my-4">
-    <div>
-      <h5 class="mb-2">هیچ کام لەم شێرپەنجانەت هەیە؟</h5>
-      <template v-for="cancer in cancers">
-        <div class="form-check form-check-inline mx-2">
-          <label class="form-check-label" :for="'hx-'+cancer.value">
-            {{ cancer.name }}
-          </label>
-          <input class="form-check-input" :id="'hx-'+cancer.value" v-model="hx" type="checkbox"
-                 :value="cancer.value">
+    <div class="mb-5">
+      <div class="row">
+        <div class="col-12 col-md-6 col-lg-4 col-xl-3 mb-3">
+          <label for="cityInput" class="form-label text-black h6">شوێنی نیشتەجێبوون</label>
+          <select v-model="city" id="cityInput" class="form-select">
+            <option v-for="city in cities" :value="city.value">{{ city.name }}</option>
+          </select>
         </div>
-      </template>
-    </div>
-    <hr class="my-4">
-    <div>
-      <h5>هیچ کام لەم شێرپەنجانە لە خیزانەکت هەیە؟</h5>
-      <p class="text-black-50 mb-2">کەسی یەکەمی خێزان کە دایک، باوک، برا و خوشک دەگرێتەوە.</p>
-      <template v-for="cancer in cancers">
-        <div class="form-check form-check-inline mx-2">
-          <label class="form-check-label" :for="'fmhx-'+cancer.value">
-            {{ cancer.name }}
-          </label>
-          <input class="form-check-input" :id="'fmhx-'+cancer.value" v-model="fmhx" type="checkbox"
-                 :value="cancer.value">
-        </div>
-      </template>
-    </div>
-    <hr class="my-4">
 
-    <div class="text-center mb-3">
-      <div class="d-grid">
+        <div class="col-12 col-md-6 col-lg-4 col-xl-2 mb-3">
+          <label for="ageInput" class="form-label text-black h6">تەمەن</label>
+          <input v-model="age" required type="number" class="form-control " id="ageInput" placeholder="تەمەن بە ژمارە">
+        </div>
+        <div class="col-12 col-md-6 col-lg-4 col-xl-2 mb-3">
+          <label for="genderInput" class="form-label text-black h6">ڕەگەز</label>
+          <select v-model="gender" id="genderInput" class="form-select">
+            <option value="male">نێر</option>
+            <option value="female">مێ</option>
+          </select>
+        </div>
+
+        <div class="col-12 col-md-6 col-lg-4 col-xl-3 mb-3">
+          <label for="maritalStatusInput" class="form-label text-black h6">دۆخی خێزانی</label>
+          <select v-model="maritalStatus" id="maritalStatusInput" class="form-select">
+            <option value="single">سەڵت</option>
+            <option value="married">خێزاندار</option>
+          </select>
+        </div>
+
+        <div class="col-12 col-md-6 col-lg-4 col-xl-2 mb-3">
+          <label for="smokingInput" class="form-label text-black h6">جگەرەت کێشاوە؟</label>
+          <select v-model="smoking" id="smokingInput" class="form-select">
+            <option value="true">بەڵی</option>
+            <option value="false">نەخێر</option>
+          </select>
+        </div>
+      </div>
+      <hr class="my-4">
+      <div>
+        <h5 class="mb-2">هیچ کام لەم شێرپەنجانەت هەیە؟</h5>
+        <template v-for="cancer in cancers">
+          <div class="form-check form-check-inline mx-2">
+            <label class="form-check-label" :for="'hx-'+cancer.value">
+              {{ cancer.name }}
+            </label>
+            <input class="form-check-input" :id="'hx-'+cancer.value" v-model="hx" type="checkbox"
+                   :value="cancer.value">
+          </div>
+        </template>
+      </div>
+      <hr class="my-4">
+      <div class="mb-4">
+        <h5>هیچ کام لەم شێرپەنجانە لە خیزانەکت هەیە؟</h5>
+        <p class="text-black-50 mb-2">کەسی یەکەمی خێزان کە دایک، باوک، برا و خوشک دەگرێتەوە.</p>
+        <template v-for="cancer in cancers">
+          <div class="form-check form-check-inline mx-2">
+            <label class="form-check-label" :for="'fmhx-'+cancer.value">
+              {{ cancer.name }}
+            </label>
+            <input class="form-check-input" :id="'fmhx-'+cancer.value" v-model="fmhx" type="checkbox"
+                   :value="cancer.value">
+          </div>
+        </template>
+      </div>
+    </div>
+
+    <div class="text-center ">
+      <div class="d-grid ">
         <button type="submit" class="btn btn-primary font-weight-bolder">
           زانینی ئەنجام
         </button>
@@ -78,12 +79,22 @@
     </div>
   </form>
 
-  <!--  <div class="alert alert-warning" role="alert">-->
-  <!--    A simple warning alert—check it out!-->
-  <!--  </div>-->
 
+  <template v-if="suggestedInvestigations.length > 0">
+    <hr class="my-4">
+    <h4 class="mb-3">پێویستە ئەم پشکنینانەی خوارەوە ئەنجام بدەیت</h4>
+    <div class="alert alert-warning" role="alert">
+      <ul class="mb-0">
+        <template v-for="investigation in investigations">
+          <li v-if="collect(suggestedInvestigations).contains(investigation.value)">{{ investigation.name }}</li>
+        </template>
+      </ul>
+    </div>
+  </template>
 
   <template v-if="suggestedHospitals.length > 0">
+    <hr class="my-4">
+    <h4 class="mb-3">دەتوانیت سەردانی ئەم نەخۆشخانەی خوارەوە بکەیت بۆ ئەنجامدانی پشکنینەکان</h4>
     <div class="alert alert-info" role="alert">
       <ul class="mb-0">
         <template v-for="hospital in hospitals">
@@ -119,7 +130,18 @@ const MAMMOGRAPHY_EVERY_TOW_YEAR = 'mammography-every-2-year';
 const MAMMOGRAPHY_ANNUALLY = 'mammography-annually';
 const CHEST_XRAY = 'c-xray';
 const PSA_OR_DRE = 'psa';
+const HPV = 'hpv-dna';
+const CITOLOGY = 'citology';
+const FIT = 'citology';
+const COLONOSCOPY_EVRY_TEN_YEAR = 'colonoscopy-every-ten-year';
+const COLONOSCOPY_EVRY_FIVE_YEAR = 'colonoscopy-every-five-year';
+const NO_TESTS_REQUIRED = 'no-tests-required';
 
+//options
+const MALE = 'male';
+const FEMALE = 'female';
+const SINGLE = 'single';
+const MARRIED = 'married';
 
 const cities = [
   {name: 'هەولێر', value: HAWLER},
@@ -127,7 +149,6 @@ const cities = [
   {name: 'دهۆک', value: DUHOK},
   {name: 'هەڵەبجە', value: SULAYMANI},
 ]
-
 const cancers = [
   {name: 'شێرپەنجەی مەمک', value: BREAST},
   {name: 'شێرپەنجەی سنگ', value: LUNG},
@@ -136,7 +157,6 @@ const cancers = [
   {name: 'شێرپەنجەی کۆلۆن', value: COLON},
   {name: 'شێرپەنجەی تر', value: OTHERS},
 ]
-
 const hospitals = [
   {id: 1, name: 'نەخۆشخانەی ڕزگاری لە هەولێر', city: HAWLER, type: ALL},
   {id: 2, name: 'کلینیکی نەخۆشیەکانی مەمک لە نەخۆشخانەی رزگاری', city: HAWLER, type: BREAST},
@@ -144,24 +164,42 @@ const hospitals = [
   {id: 4, name: 'نەخۆشخانەی شار لە سلێمانی', city: SULAYMANI, type: ALL},
   {id: 5, name: 'نەخۆشخانەی ئازادی لە دهۆک', city: DUHOK, type: ALL},
 ];
-
 const investigations = [
-  {name: 'ساڵانە خۆپشکنینی مەمک', value: SELF_EXAMINATION, type: BREAST},
-  {name: 'پشکنینی مامۆگرافی هەموو ٢ ساڵ جارێک', value: MAMMOGRAPHY_EVERY_TOW_YEAR, type: BREAST},
-  {name: 'ساڵانە پشکنینی مەمک لەلایەن پسپۆڕەوە تاکوو تەمەنی ٤٥ ساڵی', value: CLINICAL_EXAMINATION, type: BREAST},
-  {name: 'ساڵانە پشکنینی مامۆگرافی لە تەمەنی ٤٥ بۆ ٦٩ ساڵی', value: MAMMOGRAPHY_ANNUALLY, type: BREAST},
-  {name: 'ساڵانە پشکنینی مامۆگرافی لە تەمەنی ٤٥ بۆ ٦٩ ساڵی', value: MAMMOGRAPHY_ANNUALLY, type: LUNG},
+  {name: 'ساڵانە خۆپشکنینی مەمک.', value: SELF_EXAMINATION, type: BREAST},
+  {name: 'پشکنینی مامۆگرافی هەموو ٢ ساڵ جارێک.', value: MAMMOGRAPHY_EVERY_TOW_YEAR, type: BREAST},
+  {name: 'ساڵانە پشکنینی مەمک لەلایەن پسپۆڕەوە تاکوو تەمەنی ٤٥ ساڵی.', value: CLINICAL_EXAMINATION, type: BREAST},
+  {name: 'ساڵانە پشکنینی مامۆگرافی لە تەمەنی ٤٥ بۆ ٦٩ ساڵی.', value: MAMMOGRAPHY_ANNUALLY, type: BREAST},
+  {name: 'وێنەی تیشکی سنگ و سەردانی پزیشکی پسپۆڕ.', value: CHEST_XRAY, type: LUNG},
+  {
+    name: 'پشکنینی خوێنی PSA یاخود پشکنینی پرۆستات لەلایەن پزیشکی پسپۆڕەوە هەموو ١ تا ٢ ساڵ جارێک.',
+    value: PSA_OR_DRE,
+    type: PROSTATE
+  },
+  {name: 'پشکنینی HPV DNA هەموو ٥ تا ١٠ ساڵ جارێک.', value: HPV, type: CERVICAL},
+  {
+    name: 'پشکنینی خانەزانی (Cytology) هەموو ٣ ساڵ جارێک تاکو تەمەنی ٤٩ ساڵی، هەموو ٥ ساڵ جارێک لە تەمەنی ٥٠ تاکو ٦٩ ساڵی.',
+    value: CITOLOGY,
+    type: CERVICAL
+  },
+  {name: 'پشکنینی پیسایی (FIT) هەموو ساڵ جارێک.', value: FIT, type: COLON},
+  {name: 'ئەنجامدانی نازوری کۆڵۆن هەموو ١٠ ساڵ جارێک.', value: COLONOSCOPY_EVRY_TEN_YEAR, type: COLON},
+  {
+    name: 'ئەنجامدانی نازوری کۆڵۆن لە تەمەنی ٤٠ ساڵی بەرەو سەرەوە هەموو ٥ ساڵ جارێک.',
+    value: COLONOSCOPY_EVRY_FIVE_YEAR,
+    type: COLON
+  },
+  {name: 'لە ئێستادا پێویستت بە هیچ پشکنینێکی پێشوەختە نییە بۆ شێرپەنجە باوەکان.', value: NO_TESTS_REQUIRED, type: ALL},
 ];
 
-const city = ref('hawler');
-const age = ref();
-const gender = ref('male');
-const maritalStatus = ref('single');
+const city = ref(HAWLER);
+const age = ref(22);
+const gender = ref(FEMALE);
+const maritalStatus = ref(SINGLE);
 const smoking = ref(true);
 const fmhx = ref([]);
 const hx = ref([]);
 const cancerTypes = collect(cancers).pluck('value');
-const suggestedInvestigations = ref([]);
+const suggestedInvestigations = ref([SELF_EXAMINATION, CLINICAL_EXAMINATION]);
 const suggestedHospitals = ref([1, 2]);
 
 
@@ -171,7 +209,23 @@ function analyse(e) {
     return false;
   }
 
-  // check the breast cancer
+
+  // check female cancers => breast, cervical
+  if(gender.value === FEMALE) {
+    // check breast
+    // Age more than 20 => self examination
+    console.log(hx.value);
+    console.log(fmhx.value);
+  }
+
+
+  // check male cancers => prostate
+  if(gender.value === MALE) {
+    console.log('male');
+  }
+
+  // check both gender cancers => lung, colon
+
 
 }
 
